0216665b848616f458df9aece2a1ed3eb9579ec3
Jun12227 <junjunjunhyuk@gmail.com>
2025-07-18 15:55:22 +0900

    bread test

---
+++ one_hand_plus/bread.py
@@ -0,0 +1,142 @@
+import time
+import rclpy
+import DR_init
+
+# for single robot
+ROBOT_ID = "dsr01"
+ROBOT_MODEL = "m0609"
+VELOCITY, ACC = 60, 60
+
+DR_init.__dsr__id = ROBOT_ID
+DR_init.__dsr__model = ROBOT_MODEL
+
+OFF, ON = 0, 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = rclpy.create_node("rokey_force_control", namespace=ROBOT_ID)
+
+    DR_init.__dsr__node = node
+
+    try:
+        from DSR_ROBOT2 import (
+            release_compliance_ctrl,
+            release_force,
+            check_force_condition,
+            check_position_condition,
+            get_tool_force,
+            task_compliance_ctrl,
+            set_desired_force,
+            set_tool,
+            set_tcp,
+            get_current_posx,
+            set_digital_output,
+            get_digital_input,
+            amove_periodic,
+            movej,
+            movel,
+            amovel,
+            DR_FC_MOD_REL,
+            DR_MV_MOD_REL,
+            DR_MV_MOD_ABS,
+            DR_AXIS_Z,
+            DR_BASE,
+            DR_TOOL,
+        )
+
+        from DR_common2 import posx, posj
+
+    except ImportError as e:
+        print(f"Error importing DSR_ROBOT2 : {e}")
+        return
+    
+    def wait_digital_input(sig_num):
+        while not get_digital_input(sig_num):
+            time.sleep(0.5)
+            print(f"Wait for digital input: {sig_num}")
+            pass
+
+
+    def release():
+        print("set for digital output 0 1 for release")
+        set_digital_output(2, ON)
+        set_digital_output(1, OFF)
+        # wait_digital_input(2)
+        time.sleep(1.0)
+
+    def grip():
+        print("set for digital output 1 0 for grip")
+        set_digital_output(1, ON)
+        set_digital_output(2, OFF)
+        # wait_digital_input(1)
+        time.sleep(1.0)
+
+    set_tool("Tool Weight_2FG")
+    set_tcp("2FG_TCP")
+
+
+    JReady = posj([0, 0, 90, 0, 90, 0])
+    Knife = posj([0, 0, 90, 0, 90, 0])
+    Chopping = posj([0, 0, 90, 0, 90, 0])
+
+    
+    '''
+    1. 홈위치
+    2. 칼집 위치로 이동
+    3. 그리퍼 닫아서 칼집고 z축으로 들어 올리기
+    4. 빵 좌표로
+    5. 힘제어 키고 하강
+    6. 빵 만나면 periodic 비동기 실행 + z축 하강
+    7. 좌표지정위치에서 빵 썰기 멈춤 + 힘제어 끄기(순응제어는 유지)
+    8. z축으로만 이동하면서 빵 위로 오기
+    +) periodic을 활용한 빵 슬라이스 자르기 + 그리퍼 teaching(=축 회전)을 통한 슬라이스 우측으로 넘기기 + 살짝 상방이동 + 좌측이동   
+    9. 칼을 세우고 칼집 위쪽으로 이동
+    10. 천천히 하강(순응제어키고) periodic 비동기
+    11. 좌표지정위치에서 periodic 끄기
+    12. checkforce로 끝까지 밀어넣고 그리퍼 열기
+    13. 힘제어 끄고 홈위치로 이동
+
+    '''
+    while rclpy.ok():
+        release()
+        print(f"Moving to joint position: {JReady}")    
+        movej(JReady, vel=VELOCITY, acc=ACC)    # 홈위치
+
+        movej(Knife, vel=VELOCITY, acc=ACC)     # 칼집 위치로 이동
+
+        grip()      # 그리퍼 닫아서 칼집고 z축으로 들어 올리기
+        movel([0, 0, 140, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
+
+        movej(Chopping, vel=VELOCITY, acc=ACC)      # 빵 좌표로 이동
+
+        task_compliance_ctrl(stx=[1000, 500, 500, 100, 100, 100])       # 힘제어 키고 하강
+        set_desired_force(fd=[0, 0, -20, 0, 0, 0], dir=[0, 0, 1, 0, 0, 0], mod=DR_FC_MOD_REL)
+
+        while not check_force_condition(DR_AXIS_Z, max=10):      # 빵 만나면 periodic 비동기 실행
+                time.sleep(0.5)
+                pass
+        example_amp = [0, 5.0, 0.0, 0.0, 0.0, 0.0]
+        amove_periodic(amp=example_amp, period=1.0, atime=0.02, repeat=3, ref=DR_TOOL)
+
+        while not check_position_condition(DR_AXIS_Z, min=292, ref=DR_BASE):    # 좌표지정위치에서 빵 썰기 멈춤 + 힘제어 끄기(순응제어는 유지)
+                time.sleep(0.5)
+                pass
+        release_force()
+        movel([0, 0, 140, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
+        
+        
+        
+
+
+
+
+        break
+
+
+
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()

7eb025905a268d5646f90735d7a063d42cd8bf1d
AdleRami <jhjy96@naver.com>
2025-07-20 15:24:43 +0900

    bread_ffff

--- one_hand_plus/bread.py
+++ one_hand_plus/bread.py
@@ -21,6 +21,7 @@ def main(args=None):
 
     try:
         from DSR_ROBOT2 import (
+            set_ref_coord,
             release_compliance_ctrl,
             release_force,
             check_force_condition,
@@ -77,17 +78,19 @@ def main(args=None):
 
     set_tool("Tool Weight_2FG")
     set_tcp("2FG_TCP")
-
+# 96.45
+# 50.59
 
     JReady = posj([0, 0, 90, 0, 90, 0])
 
     Knife = posj([-3.59, 19.83, 119.88, -2.42, -49.43, 91.43])
     mov_1 = posj([-11.27, 6.04, 106.79, -23.6, -24.34, 111.6])
     Chopping = posj([-26.24, 38.36, 60.44, -0.14, 81.4, 154.61])
+    Slope_knife = posj([-18.16, 32.62, 66.16, -16.32, 76.96,165.61])
     Slice_push = posj([-31.52, 44.3, 53.66, -0.25, 82.03,149.44])
     Bread_push = posj([-31.52, 44.3, 53.66, -0.25, 82.03,149.44]) ################################ 사용할 시 티칭 필요 #########################
     Vertical_knife = posj([-27.49, 3.4, 89.29, -8.28, 2.97,99.88])
-    Upper_knife = posj([-3.24, -12.65, 100.93, -4.09, -23.28, 93.76])
+    Upper_knife = posj([-3.24, 12.65, 100.93, -4.09, -23.28, 93.76])
     ##########################################
     x_Knife = posx([0, 0, 90, 0, 90, 0])
     x_Chopping = posx([0, 0, 90, 0, 90, 0])
@@ -130,79 +133,89 @@ def main(args=None):
         movel([0, 0, 160, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
 
         ## fix
-        movesj([mov_1, Chopping], vel=VELOCITY, acc=ACC)
+        movesj([mov_1, Chopping, Slope_knife], vel=VELOCITY, acc=ACC)
         # movej(mov_1, vel=VELOCITY, acc=ACC)
         # movej(Chopping, vel=VELOCITY, acc=ACC)
         time.sleep(0.1)
 
-        for _ in range(1):
+        # for _ in range(1):
+        # 빵 좌표로 이동
+        # movej(Chopping, vel=VELOCITY, acc=ACC)      
+        # movel([0, 0, -10, 0, -20, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
 
-            movej(Chopping, vel=VELOCITY, acc=ACC)      # 빵 좌표로 이동
-            movel([0, 0, -10, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
+        task_compliance_ctrl(stx=[1000, 1000, 300, 100, 100, 100])       # 힘제어 키고 하강
+        time.sleep(0.5)
+        set_desired_force(fd=[0, 0, -10, 0, 0, 0], dir=[0, 0, 1, 0, 0, 0], mod=DR_FC_MOD_REL)
 
-            task_compliance_ctrl(stx=[1000, 1000, 300, 100, 100, 100])       # 힘제어 키고 하강
+        # while not check_force_condition(DR_AXIS_Z, max=11):      # 빵 만나면 periodic 비동기 실행
+        #     print("Starting check_force_condition")
+        #     time.sleep(0.5)
+        #     pass
+        while not check_position_condition(DR_AXIS_Z, min=84.50, ref=DR_BASE):
+            print("Starting check_position_condition")
             time.sleep(0.5)
-            set_desired_force(fd=[0, 0, -10, 0, 0, 0], dir=[0, 0, 1, 0, 0, 0], mod=DR_FC_MOD_REL)
+            pass
+        
+        periodic_amp = [0, 15.0, 0.0, 0.0, 0.0, 0.0]
+        amove_periodic(amp=periodic_amp, period=1.0, atime=0.02, repeat=10, ref=DR_TOOL)
 
-            # while not check_force_condition(DR_AXIS_Z, max=11):      # 빵 만나면 periodic 비동기 실행
-            #     print("Starting check_force_condition")
-            #     time.sleep(0.5)
-            #     pass
-            while not check_position_condition(DR_AXIS_Z, min=46.50, ref=DR_BASE):
-                print("Starting check_position_condition")
-                time.sleep(0.5)
-                pass
+        while not check_position_condition(DR_AXIS_Z, min=45.00, ref=DR_BASE):    # 좌표지정위치에서 빵 썰기 멈춤 + 힘제어 끄기(순응제어는 유지)
+            print("Periodic check_position_condition")
+            print(get_current_posx())
+            time.sleep(0.5)
+            pass
+        release_force()
+        time.sleep(0.5)
+        release_compliance_ctrl()
+        
+        
+        set_ref_coord(DR_BASE)
+        movel([0, 0, 120, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)      # z축으로만 이동하면서 빵 위로 나온 뒤 순응제어 종료
+        
+
+        # movej(Slice_push, vel=VELOCITY, acc=ACC)     # 자른 빵을 밀 위치로 이동 후 순응제어 키고 밀기
+        # task_compliance_ctrl(stx=[1000, 500, 1000, 100, 100, 100]) 
+        # movel([0, 130, 0, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
+        # release_compliance_ctrl()
+
+        # movej(Bread_push, vel=VELOCITY, acc=ACC)     # 빵본체를 밀 위치로 이동 후 순응제어 키고 밀기
+        # task_compliance_ctrl(stx=[500, 1000, 1000, 100, 100, 100]) 
+        # movel([140, 0, 0, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
+        # release_compliance_ctrl()
             
-            periodic_amp = [0, 30.0, 0.0, 0.0, 0.0, 0.0]
-            amove_periodic(amp=periodic_amp, period=1.0, atime=0.02, repeat=20, ref=DR_TOOL)
-
-            while not check_position_condition(DR_AXIS_Z, min=22.50, ref=DR_BASE):    # 좌표지정위치에서 빵 썰기 멈춤 + 힘제어 끄기(순응제어는 유지)
-                print("Periodic check_position_condition")
-                print(get_current_posx())
-                time.sleep(0.5)
-                pass
-            release_force()
-            drl_script_stop(DR_SSTOP)
-
-            movel([0, 0, 120, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)      # z축으로만 이동하면서 빵 위로 나온 뒤 순응제어 종료
-            release_compliance_ctrl()
-
-            # movej(Slice_push, vel=VELOCITY, acc=ACC)     # 자른 빵을 밀 위치로 이동 후 순응제어 키고 밀기
-            # task_compliance_ctrl(stx=[1000, 500, 1000, 100, 100, 100]) 
-            # movel([0, 130, 0, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
-            # release_compliance_ctrl()
-
-            # movej(Bread_push, vel=VELOCITY, acc=ACC)     # 빵본체를 밀 위치로 이동 후 순응제어 키고 밀기
-            # task_compliance_ctrl(stx=[500, 1000, 1000, 100, 100, 100]) 
-            # movel([140, 0, 0, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
-            # release_compliance_ctrl()
         
         # 칼 세우고 칼집 위치로 이동
         movej(Vertical_knife, vel=VELOCITY, acc=ACC)     
         movej(Upper_knife, vel=VELOCITY, acc=ACC)
 
         # 천천히 하강(순응제어키고) periodic 비동기
-        task_compliance_ctrl(stx=[1000, 1000, 500, 100, 100, 100])
-        periodic_amp_1 = [10.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        amove_periodic(amp=periodic_amp_1, period=3.0, atime=0.02, repeat=2, ref=DR_TOOL)
-        periodic_amp_2 = [0, 5.0, 0.0, 0.0, 0.0, 0.0]
-        amove_periodic(amp=periodic_amp_2, period=1.0, atime=0.02, repeat=6, ref=DR_TOOL)
+        # set_ref_coord(DR_BASE)
+        # periodic_amp_2 = [0, 5.0, 0.0, 0.0, 0.0, 0.0]
+        # amove_periodic(amp=periodic_amp_2, period=1.0, atime=0.02, repeat=2, ref=DR_TOOL)
+        # task_compliance_ctrl(stx=[1000, 1000, 500, 100, 100, 100])
+        # set_desired_force(fd=[0, 0, -10, 0, 0, 0], dir=[0, 0, 1, 0, 0, 0], mod=DR_FC_MOD_REL)
+        # # periodic_amp_1 = [10.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        # # amove_periodic(amp=periodic_amp_1, period=3.0, atime=0.02, repeat=2, ref=DR_TOOL)
 
-        # 좌표지정위치에서 periodic 끄기
-        while not check_position_condition(DR_AXIS_Z, min=372.63, ref=DR_BASE):
-            time.sleep(0.5)
-            pass
-        drl_script_stop(DR_SSTOP)
 
-        # checkforce로 끝까지 밀어넣고 그리퍼 열기
-        set_desired_force(fd=[0, 0, -20, 0, 0, 0], dir=[0, 0, 1, 0, 0, 0], mod=DR_FC_MOD_REL)
-        while not check_force_condition(DR_AXIS_Z, max=15):
-                time.sleep(0.5)
-                pass
-        release_force()
+        # # 좌표지정위치에서 periodic 끄기
+        # while not check_position_condition(DR_AXIS_Z, min=372.63, ref=DR_BASE):
+        #     print("Periodic check_position_condition")
+        #     time.sleep(0.5)
+        #     pass
+
+        # # checkforce로 끝까지 밀어넣고 그리퍼 열기
+        # while not check_force_condition(DR_AXIS_Z, max=15):
+        #         time.sleep(0.5)
+        #         print("check_force_condition")
+        #         pass
+        # release_force()
+        
+        # release_compliance_ctrl()
+
+        movel([0, 0, -130, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
         release()
-        release_compliance_ctrl()
-
+        movel([-50, 0, 0, 0, 0, 0], vel=VELOCITY, acc=ACC, ref=DR_BASE, mod=DR_MV_MOD_REL)
         movej(JReady, vel=VELOCITY, acc=ACC)    # 홈위치
         break
     rclpy.shutdown()
